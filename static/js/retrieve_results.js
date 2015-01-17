17fdc6280959f29451038c233100b3ad1259654f
Tony Wu <twu224@uwo.ca>
2015-01-16 23:06:08 -0500

    Fixed CSRF for Chrome
    Chrome CSRF stopped authenticating when Chrome updated. Fixed this bug.

--- static/js/retrieve_results.js
+++ static/js/retrieve_results.js
@@ -20,7 +20,11 @@ $(document).ready(function() {
 	    // setup some local variables
 	    var $form = $('#participant_form');
 
+	    //CSRF Cookie madness
+		var csrftoken = getCookie('csrftoken');
+
 	    // Let's select and cache all the fields
+	    $('input[name=csrfmiddlewaretoken]').val( csrftoken );
 	    var $inputs = $form.find("input, select, button");
 	    var $data = $inputs.serialize();
 
@@ -28,44 +32,6 @@ $(document).ready(function() {
 	    // Note: we disable elements AFTER the form data has been serialized.
 	    // Disabled form elements will not be serialized.
 	    $inputs.prop("disabled", true);
-
-	    //CSRF Cookie madness
-	    function getCookie(name) {
-		    var cookieValue = null;
-		    if (document.cookie && document.cookie != '') {
-		        var cookies = document.cookie.split(';');
-		        for (var i = 0; i < cookies.length; i++) {
-		            var cookie = jQuery.trim(cookies[i]);
-		            // Does this cookie string begin with the name we want?
-		            if (cookie.substring(0, name.length + 1) == (name + '=')) {
-		                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-		                break;
-		            }
-		        }
-		    }
-		    return cookieValue;
-		}
-
-		var csrftoken = getCookie('csrftoken');
-
-		function csrfSafeMethod(method) {
-		    // these HTTP methods do not require CSRF protection
-		    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-		}
-
-		function sameOrigin(url) {
-		    // test that a given url is a same-origin URL
-		    // url could be relative or scheme relative or absolute
-		    var host = document.location.host; // host + port
-		    var protocol = document.location.protocol;
-		    var sr_origin = '//' + host;
-		    var origin = protocol + sr_origin;
-		    // Allow absolute or scheme relative URLs to same origin
-		    return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
-		        (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
-		        // or any other URL that isn't scheme relative or absolute i.e relative.
-		        !(/^(\/\/|http:|https:).*/.test(url));
-		}
 
 		$.ajaxSetup({
 		    beforeSend: function(xhr, settings) {
@@ -128,7 +94,11 @@ $(document).ready(function() {
 	    // setup some local variables
 	    var $form = $('#admin_form');
 
+	    //CSRF Cookie madness
+		var csrftoken = getCookie('csrftoken');
+
 	    // Let's select and cache all the fields
+	    $('input[name=csrfmiddlewaretoken]').val( csrftoken );
 	    var $inputs = $form.find("input, select, button");
 	    var $data = $inputs.serialize();
 
@@ -136,44 +106,6 @@ $(document).ready(function() {
 	    // Note: we disable elements AFTER the form data has been serialized.
 	    // Disabled form elements will not be serialized.
 	    $inputs.prop("disabled", true);
-
-	    //CSRF Cookie madness
-	    function getCookie(name) {
-		    var cookieValue = null;
-		    if (document.cookie && document.cookie != '') {
-		        var cookies = document.cookie.split(';');
-		        for (var i = 0; i < cookies.length; i++) {
-		            var cookie = jQuery.trim(cookies[i]);
-		            // Does this cookie string begin with the name we want?
-		            if (cookie.substring(0, name.length + 1) == (name + '=')) {
-		                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-		                break;
-		            }
-		        }
-		    }
-		    return cookieValue;
-		}
-
-		var csrftoken = getCookie('csrftoken');
-
-		function csrfSafeMethod(method) {
-		    // these HTTP methods do not require CSRF protection
-		    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-		}
-
-		function sameOrigin(url) {
-		    // test that a given url is a same-origin URL
-		    // url could be relative or scheme relative or absolute
-		    var host = document.location.host; // host + port
-		    var protocol = document.location.protocol;
-		    var sr_origin = '//' + host;
-		    var origin = protocol + sr_origin;
-		    // Allow absolute or scheme relative URLs to same origin
-		    return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
-		        (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
-		        // or any other URL that isn't scheme relative or absolute i.e relative.
-		        !(/^(\/\/|http:|https:).*/.test(url));
-		}
 
 		$.ajaxSetup({
 		    beforeSend: function(xhr, settings) {
@@ -220,3 +152,38 @@ $(document).ready(function() {
 	    return false;
 	});
 });
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+function sameOrigin(url) {
+    // test that a given url is a same-origin URL
+    // url could be relative or scheme relative or absolute
+    var host = document.location.host; // host + port
+    var protocol = document.location.protocol;
+    var sr_origin = '//' + host;
+    var origin = protocol + sr_origin;
+    // Allow absolute or scheme relative URLs to same origin
+    return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
+        (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
+        // or any other URL that isn't scheme relative or absolute i.e relative.
+        !(/^(\/\/|http:|https:).*/.test(url));
+}
