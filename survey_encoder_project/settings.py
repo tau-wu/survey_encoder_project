b22e55b08031c2e47d7ed0a1766e8378b25d4bd2
Tony Wu <twu224@uwo.ca>
2015-02-25 00:35:06 -0500

    Changed Settings
    Updated Settings so site can only be run on allowed hosts

--- survey_encoder_project/settings.py
+++ survey_encoder_project/settings.py
@@ -22,11 +22,11 @@ STATIC_PATH = os.path.join(BASE_DIR, 'static')
 SECRET_KEY = 'h=_c&ynmp$^iv2f#ai2xlqavp%^v06t318q382_u5@c=wd1@!o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [tauwu.pythonanywhere.com]
 
 
 # Application definition
