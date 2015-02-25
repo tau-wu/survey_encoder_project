51a32c56a3abe620e15c6207cc0616abd3c202b1
Tony Wu <twu224@uwo.ca>
2015-02-25 01:04:35 -0500

    Another Test

--- survey_encoder_project/settings.py
+++ survey_encoder_project/settings.py
@@ -22,7 +22,7 @@ STATIC_PATH = os.path.join(BASE_DIR, 'static')
 SECRET_KEY = 'h=_c&ynmp$^iv2f#ai2xlqavp%^v06t318q382_u5@c=wd1@!o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 TEMPLATE_DEBUG = True
 
